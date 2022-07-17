5f12d8281d349ae382f6b38f13b45c3f20e1cfc9
mohamed abouda <m.abouda@chefaa.com>
2022-07-17 15:41:14 +0200

    add sessions and cookies usage

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -22,7 +24,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cookieParser());
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+}))
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -35,3 +43,25 @@ app.use(errorController.renderError);
 module.exports = app;
 
 sync.syncTables()
+
+const Product = require('./models/product')
+
+// Product.findByPk({
+//     title: 'title',
+//     price: 500,
+//     image_url: 'htllofjadlkf',
+//     description: 'description'
+// }).then(result => {
+//      console.log(result)
+// }).catch(err => {
+//     console.log(err)
+// });
+// var product = 1;
+// Product.findByPk(2).then(result => {
+//     result.destroy();
+//     console.log(result)
+// });
+// setTimeout(function() {
+//     console.log("Executed after 1 second");
+// }, 1000);
+// console.log(product)
